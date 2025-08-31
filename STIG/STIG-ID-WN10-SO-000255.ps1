9ddf59a51d81970978614a31ebf24fb49b7a5efd
vlutumba <138389312+vlutumba@users.noreply.github.com>
2025-08-31 16:47:03 -0500

    Create STIG-ID-WN10-SO-000255.ps1

---
+++ STIG/STIG-ID-WN10-SO-000255.ps1
@@ -0,0 +1,45 @@
+ <#
+.SYNOPSIS
+    This PowerShell script ensures that the User Account Control automatically denies elevation requests for standard users.
+
+.NOTES
+    Author          : Vilfride Lutumba
+    LinkedIn        : linkedin.com/in/vlutumba/
+    GitHub          : github.com/vlutumba
+    Date Created    : 2025-08-31
+    Last Modified   : 2025-08-31
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000255
+
+.TESTED ON
+    Date(s) Tested  : 2025-08-31
+    Tested By       : Vilfride Lutumba
+    Systems Tested  : Windows 10 Pro 22H2
+    PowerShell Ver. : 5.1 build 19041 Rev. 6216
+
+.USAGE
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-SO-000255.ps1 
+#>
+
+# Define the registry path and value
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$valueName = "ConsentPromptBehaviorUser"
+$valueData = 0  # 0x00000000 in hexadecimal
+$valueType = "DWord"
+
+# Define the registry path
+#$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
+
+# Make sure the path exists
+If (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the RequireSecuritySignature value (0x00000000 = 0 in decimal)
+Set-ItemProperty -Path $regPath -Name $valueName -Value $valueData -Type $valueType
+
+# Verify the value
+Get-ItemProperty -Path $regPath | Select-Object $valueName
