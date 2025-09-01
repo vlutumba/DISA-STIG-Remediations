193ae98f7bbc37f3505a270c65d6bce030b18462
vlutumba <138389312+vlutumba@users.noreply.github.com>
2025-09-01 14:26:53 -0500

    Create STIG-ID-WN10-CC-000200.ps1

---
+++ STIG/STIG-ID-WN10-CC-000200.ps1
@@ -0,0 +1,42 @@
+ <#
+.SYNOPSIS
+    This PowerShell script ensures that the Administrator accounts are not enumerated during elevation..
+
+.NOTES
+    Author          : Vilfride Lutumba
+    LinkedIn        : linkedin.com/in/vlutumba/
+    GitHub          : github.com/vlutumba
+    Date Created    : 2025-09-01
+    Last Modified   : 2025-09-01
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000200
+
+.TESTED ON
+    Date(s) Tested  : 2025-09-01
+    Tested By       : Vilfride Lutumba
+    Systems Tested  : Windows 10 Pro 22H2
+    PowerShell Ver. : 5.1 build 19041 Rev. 6216
+
+.USAGE
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000200.ps1 
+#>
+
+# Define the registry path and value
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\CredUI"
+$valueName = "EnumerateAdministrators"
+$valueData = 0  # 0x00000000 in hexadecimal
+$valueType = "DWord"
+
+# Make sure the path exists
+If (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the EnumerateAdministrators value (0x00000000 = 0 in decimal)
+Set-ItemProperty -Path $regPath -Name $valueName -Value $valueData -Type $valueType
+
+# Verify the value
+Get-ItemProperty -Path $regPath | Select-Object $valueName
